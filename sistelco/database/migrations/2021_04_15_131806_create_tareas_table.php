61057f645364f2c2f06461305e69e45a10bd4d05
Diego Urdanigo <diegourdanigo@gmail.com>
2021-04-15 10:25:47 -0500

    prueba git

---
+++ sistelco/database/migrations/2021_04_15_131806_create_tareas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nombretarea');
+            $table->string('estado');
+            $table->string('asigpor');
+            $table->string('asigpara');
+            $table->datetime('fechaasig');
+            $table->datetime('fechaentrega');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+}
